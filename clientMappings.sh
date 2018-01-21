da9b6070a40310df38aefe33541ba64f3cd0b2b3
Jadon Fowler <jadonflower@gmail.com>
2018-01-21 00:53:44 -0800

    Start on compilation
    FernFlower was updated.
    
    The mappings were updated.
    
    Some decompilation bugs were fixed.
    
    Signed-off-by: Jadon Fowler <jadonflower@gmail.com>

--- clientMappings.sh
+++ clientMappings.sh
@@ -4,7 +4,7 @@ java -XX:+UseG1GC -jar lib/SpecialSource.jar map -i "minecraft/b1_7_3_client.jar
 echo "Decompiling mapped jar"
 rm -rf minecraft/src/
 mkdir -p minecraft/src/
-java -XX:+UseG1GC -Xmx5G -jar lib/fernflower.jar "-dgs=1" "-hdc=0" "-asc=1" "-udv=0" "-din=1" "-rbr=0" "-rsy=1" "minecraft/b1_7_3_client_mapped.jar" "minecraft/src/" | awk '$4 ~ "minecraft" {printf "Decompiling %s\r", $4}'
+java -XX:+UseG1GC -Xmx7G -jar lib/fernflower.jar "-dgs=1" "-hdc=0" "-asc=1" "-udv=0" "-din=1" "-rbr=0" "-rsy=1" "minecraft/b1_7_3_client_mapped.jar" "minecraft/src/" | awk '$4 ~ "minecraft" {printf "Decompiling %s\r", $4}'
 
 echo "Extracting sources"
 pushd minecraft/src/
@@ -13,6 +13,7 @@ rm -rf META-INF
 popd
 
 echo "Moving source files"
+rm -rf src/main/java/net/minecraft/
 # cp minecraft/src/*.java src/main/java/ # Unmapped files
 cp -r minecraft/src/net/ src/main/java/
 
